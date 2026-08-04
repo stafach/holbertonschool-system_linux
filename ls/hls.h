d6abe74dd0afd04be548c4fd401df2c2ebcb1fea
chermat mustapha <mchermat@outlook.fr>
2026-08-04 12:03:05 +0200

    add all prototype of functions

--- ls/hls.h
+++ ls/hls.h
@@ -1,13 +1,20 @@
 #ifndef HLS_H
 #define HLS_H
 
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <dirent.h>
+#include <errno.h>
 
-int compare(const void *a, const void *b);
-int read_names(DIR *d, char ***names, int *count);
+int count_entries(DIR *d);
+int fill_names(DIR *d, char **names);
+
+int my_strlen(const char *s);
+char *my_strdup(const char *src);
+int my_strcasecmp(const char *s1, const char *s2);
+
+void sort_names(char **names, int count);
 void print_names(char **names, int count);
+void free_names(char **names, int count);
 
-#endif /* HLS_H */
+#endif
