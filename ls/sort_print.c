33c75fc877b1ee619593dbcfd00911f6addff9bd
chermat mustapha <mchermat@outlook.fr>
2026-08-04 12:07:45 +0200

    create fuction for sort the entries, one functin for print the entries and an other for free the memory

---
+++ ls/sort_print.c
@@ -0,0 +1,57 @@
+#include "hls.h"
+
+/**
+* sort_names - Sort an array of strings alphabetically (bubble sort)
+* @names: array of strings to sort
+* @count: number of elements in the array
+*/
+
+void sort_names(char **names, int count)
+{
+	int i, j;
+	char *tmp;
+
+	for (i = 0; i < count - 1; i++)
+	{
+		for (j = 0; j < count - i - 1; j++)
+		{
+			if (my_strcasecmp(names[j], names[j + 1]) > 0)
+			{
+				tmp = names[j];
+				names[j] = names[j + 1];
+				names[j + 1] = tmp;
+			}
+		}
+	}
+}
+
+/**
+* print_names - Print each name on its own line
+* @names: array of strings to print
+* @count: number of elements in the array
+*/
+
+void print_names(char **names, int count)
+{
+	int i;
+
+	for (i = 0; i < count - 1; i++)
+		printf("%s  ", names[i]);
+	printf("%s\n", names[count - 1]);
+}
+
+
+/**
+* free_names - Free an array of strings and the array itself
+* @names: array of strings to free
+* @count: number of elements in the array
+*/
+
+void free_names(char **names, int count)
+{
+	int i;
+
+	for (i = 0; i < count; i++)
+		free(names[i]);
+	free(names);
+}
