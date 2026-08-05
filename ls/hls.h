7f6470a4413a2161140e0b2f08efd02149a28456
chermat mustapha <mchermat@outlook.fr>
2026-08-05 08:09:29 +0200

    add prototype of strerror

--- ls/hls.h
+++ ls/hls.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/stat.h>
 
 int count_entries(DIR *d);
 int fill_names(DIR *d, char **names);
@@ -13,6 +14,7 @@ int fill_names(DIR *d, char **names);
 int my_strlen(const char *s);
 char *my_strdup(const char *src);
 int my_strcasecmp(const char *s1, const char *s2);
+const char *my_strerror(int err);
 
 void sort_names(char **names, int count);
 void print_names(char **names, int count);
