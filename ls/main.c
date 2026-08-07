fff0f23774f06da7fd5ec6e2089bdc2a3ab8eb8c
chermat mustapha <mchermat@outlook.fr>
2026-08-07 18:27:52 +0200

    create init_option and process_path option, change main for betty style

--- ls/main.c
+++ ls/main.c
@@ -1,37 +1,63 @@
 #include "hls.h"
 
 /**
-* main - Entry point
-* @argc: argument count
-* @argv: argument vector
-* Return: 0 on success, 1 or 2 on error
-*/
-
-int main(int argc, char **argv)
+ * init_options - Initialize program options
+ * @opts: program options
+ */
+void init_options(options *opts)
 {
-	int i, ret, status, count;
-	char **names;
+	opts->long_format = 0;
+	opts->all = 0;
+}
 
-	status = 0;
-	if (argc == 1)
-		return (list_directory(".", argv[0], 0));
+/**
+ * process_paths - Process all paths given by the user
+ * @argc: argument count
+ * @argv: argument vector
+ * @first_path: index of first path
+ * @opts: program options
+ * Return: program status
+ */
+int process_paths(int argc, char **argv,
+		int first_path, options *opts)
+{
+	int i;
+	int ret;
+	int status = 0;
 
-	count = argc - 1;
-	names = malloc(sizeof(char *) * count);
-	if (names == NULL)
-		return (1);
-
-	for (i = 0; i < count; i++)
-		names[i] = argv[i + 1];
-	sort_names(names, count);
-	for (i = 0; i < count; i++)
+	for (i = first_path; i < argc; i++)
 	{
-		if (i > 0)
+		if (i > first_path)
 			printf("\n");
-		ret = list_directory(names[i], argv[0], count > 1);
+
+		ret = list_directory(argv[i], argv[0],
+				argc - first_path > 1, opts);
+
 		if (ret > status)
 			status = ret;
 	}
-	free(names);
+
 	return (status);
+}
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: program status
+ */
+int main(int argc, char **argv)
+{
+	options opts;
+	int first_path;
+
+	init_options(&opts);
+
+	if (parse_options(argc, argv, &opts, &first_path) != 0)
+		return (1);
+
+	if (first_path >= argc)
+		return (list_directory(".", argv[0], 0, &opts));
+
+	return (process_paths(argc, argv, first_path, &opts));
 }
