4d58cf328bf73cb4d4ad3643c428bdbd25074eb9
chermat mustapha <mchermat@outlook.fr>
2026-09-26 00:41:37 +0200

    del doble

--- readelf/elf_header.c
+++ readelf/elf_header.c
@@ -200,3 +200,25 @@ void print_header64(Elf64_Ehdr *header)
 {
 	printf("ELF Header:\n");
 	print_common(header->e_ident, header->e_type,
+		     header->e_machine, header->e_version);
+
+	printf("Entry point address:               0x%lx\n",
+	       header->e_entry);
+	printf("Start of program headers:          %lu (bytes into file)\n",
+	       header->e_phoff);
+	printf("Start of section headers:          %lu (bytes into file)\n",
+	       header->e_shoff);
+	printf("Flags:                             0x%x\n", header->e_flags);
+	printf("Size of this header:               %u (bytes)\n",
+	       header->e_ehsize);
+	printf("Size of program headers:           %u (bytes)\n",
+	       header->e_phentsize);
+	printf("Number of program headers:         %u\n",
+	       header->e_phentsize);
+	printf("Size of section headers:           %u (bytes)\n",
+	       header->e_shentsize);
+	printf("Number of section headers:         %u\n",
+	       header->e_shnum);
+	printf("Section header string table index: %u\n",
+	       header->e_shstrndx);
+}
